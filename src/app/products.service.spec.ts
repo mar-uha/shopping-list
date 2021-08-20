bb456be6f30420a16f1d9073182babc0d87849a4
Marc UHART <MUT@dimosoftware.com>
2021-08-20 16:27:18 +0200

    Adding service unit test.

--- src/app/products.service.spec.ts
+++ src/app/products.service.spec.ts
@@ -1,16 +1,84 @@
 import { TestBed } from '@angular/core/testing';
+import { Product } from './models/product';
 
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
-  let service: ProductsService;
+  let productsService: ProductsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductsService);
+    productsService = TestBed.inject(ProductsService);
+    let store = {} as any;
+    spyOn(localStorage, 'getItem').and.callFake(function (key) {
+      return store[key];
+    });
+    spyOn(localStorage, 'setItem').and.callFake(function (key, value) {
+      return store[key] = value + '';
+    });
+    spyOn(localStorage, 'clear').and.callFake(function () {
+        store = {};
+    });
+
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(productsService).toBeTruthy();
+  });
+
+  it('should return an Array of 3 products', () => {
+    let actualProducts: Array<Product> = [];
+
+    const hardcodedProducts = [
+      {
+        name: 'Carrot',
+        isSelected: false,
+        count: 0
+      },
+      {
+        name: 'Eggs',
+        isSelected: true,
+        count: 6
+      },
+      {
+        name: 'Mustard',
+        isSelected: true,
+        count: 1
+      }] as Array<Product>;
+
+    localStorage.setItem('allProducts', JSON.stringify(hardcodedProducts));
+    actualProducts = productsService.list();
+
+    expect(actualProducts.length).withContext('The `list` method should return an array of Product').not.toBe(0);
+    expect(actualProducts.length).withContext('The `list` method should return an array of 3 Products').toBe(3);
+    expect(actualProducts).toEqual(hardcodedProducts);
+  });
+
+  it('should create a product', () => {
+    let actualProducts: Array<Product> = [];
+
+    const actualProduct ={
+        name: 'Carrot',
+        isSelected: false,
+        count: 0
+      } as Product;
+
+    actualProducts = productsService.create(actualProduct);
+
+    expect(actualProducts.length).withContext('The `create` method should return an array of Product').not.toBe(0);
+    expect(actualProducts.length).withContext('The `create` method should return an array of 1 Product').toBe(1);
+    expect(actualProducts[0].name).toEqual('Carrot');
+    expect(actualProducts[0].isSelected).toEqual(false);
+    expect(actualProducts[0].count).toEqual(0);
+
+    const localStorageValue = localStorage.getItem('allProducts');
+    expect(localStorageValue).not.toBeNull();
+    expect(localStorageValue?.length).not.toEqual(0);
+    if (localStorageValue) {
+      const productsInLocalStorage = JSON.parse(localStorageValue) as Array<Product>;
+      expect(productsInLocalStorage[0].name).toEqual('Carrot');
+      expect(productsInLocalStorage[0].isSelected).toEqual(false);
+      expect(productsInLocalStorage[0].count).toEqual(0);
+    }
   });
 });
