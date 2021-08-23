a1e54a68543868474171487b3098436fa0c9d5eb
Marc UHART <MUT@dimosoftware.com>
2021-08-23 10:19:54 +0200

    Adding unit test on products.service.

--- src/app/products.service.spec.ts
+++ src/app/products.service.spec.ts
@@ -81,4 +81,124 @@ describe('ProductsService', () => {
       expect(productsInLocalStorage[0].count).toEqual(0);
     }
   });
+
+  it('should delete a product', () => {
+    let actualProducts: Array<Product> = [];
+
+    const hardcodedProducts = [
+      {
+        name: 'Carrot',
+        isSelected: false,
+        count: 0
+      },
+      {
+        name: 'Eggs',
+        isSelected: true,
+        count: 6
+      },
+      {
+        name: 'Mustard',
+        isSelected: true,
+        count: 1
+      }] as Array<Product>;
+
+    localStorage.setItem('allProducts', JSON.stringify(hardcodedProducts));
+
+    actualProducts = productsService.delete('Carrot');
+
+    expect(actualProducts.length).withContext('The `create` method should return an array of Product').not.toBe(0);
+    expect(actualProducts.length).withContext('The `create` method should return an array of 2 Products').toBe(2);
+    expect(actualProducts[0].name).toEqual('Eggs');
+    expect(actualProducts[0].isSelected).toEqual(true);
+    expect(actualProducts[0].count).toEqual(6);
+
+    const localStorageValue = localStorage.getItem('allProducts');
+    expect(localStorageValue).not.toBeNull();
+    expect(localStorageValue?.length).not.toEqual(0);
+    if (localStorageValue) {
+      const productsInLocalStorage = JSON.parse(localStorageValue) as Array<Product>;
+      expect(productsInLocalStorage.filter(p => p.name === 'Carrot').length).toBe(0);
+    }
+  });
+
+  it('should rename a product', () => {
+    let actualProducts: Array<Product> = [];
+
+    const hardcodedProducts = [
+      {
+        name: 'Carrot',
+        isSelected: false,
+        count: 0
+      },
+      {
+        name: 'Eggs',
+        isSelected: true,
+        count: 6
+      },
+      {
+        name: 'Mustard',
+        isSelected: true,
+        count: 1
+      }] as Array<Product>;
+
+    localStorage.setItem('allProducts', JSON.stringify(hardcodedProducts));
+
+    actualProducts = productsService.rename('Carrot', 'Burger');
+
+    expect(actualProducts.length).withContext('The `create` method should return an array of Product').not.toBe(0);
+    expect(actualProducts.length).withContext('The `create` method should return an array of 3 Products').toBe(3);
+    expect(actualProducts[0].name).toEqual('Burger');
+    expect(actualProducts[0].isSelected).toEqual(false);
+    expect(actualProducts[0].count).toEqual(0);
+
+    const localStorageValue = localStorage.getItem('allProducts');
+    expect(localStorageValue).not.toBeNull();
+    expect(localStorageValue?.length).not.toEqual(0);
+    if (localStorageValue) {
+      const productsInLocalStorage = JSON.parse(localStorageValue) as Array<Product>;
+      expect(productsInLocalStorage[0].name).toEqual('Burger');
+      expect(productsInLocalStorage[0].isSelected).toEqual(false);
+      expect(productsInLocalStorage[0].count).toEqual(0);
+    }
+  });
+
+  it('should save a product', () => {
+    let actualProducts: Array<Product> = [];
+
+    const hardcodedProducts = [
+      {
+        name: 'Carrot',
+        isSelected: false,
+        count: 0
+      },
+      {
+        name: 'Eggs',
+        isSelected: true,
+        count: 6
+      },
+      {
+        name: 'Mustard',
+        isSelected: true,
+        count: 1
+      }] as Array<Product>;
+
+    actualProducts = productsService.save(hardcodedProducts);
+
+    expect(actualProducts.length).withContext('The `create` method should return an array of Product').not.toBe(0);
+    expect(actualProducts.length).withContext('The `create` method should return an array of 3 Products').toBe(3);
+    expect(actualProducts[0].name).toEqual('Carrot');
+    expect(actualProducts[0].isSelected).toEqual(false);
+    expect(actualProducts[0].count).toEqual(0);
+
+    const localStorageValue = localStorage.getItem('allProducts');
+    expect(localStorageValue).not.toBeNull();
+    expect(localStorageValue?.length).not.toEqual(0);
+    if (localStorageValue) {
+      const productsInLocalStorage = JSON.parse(localStorageValue) as Array<Product>;
+      expect(productsInLocalStorage[0].name).toEqual('Carrot');
+      expect(productsInLocalStorage[0].isSelected).toEqual(false);
+      expect(productsInLocalStorage[0].count).toEqual(0);
+    }
+  });
+  
 });
